6dfee3ed67abf67fcf659a8bc3a7e69c684a39d8
Nate Wright <natew@notthisway.com>
2016-11-10 15:19:49 +0000

    Support pages in content template part

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,7 +11,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_single() ) :
+		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
@@ -26,7 +26,7 @@
 
 	<div class="entry-content">
 		<?php
-			if ( is_single() ) {
+			if ( is_singular() ) {
 				the_content();
 				wp_link_pages( array(
 					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'totcbase' ),
